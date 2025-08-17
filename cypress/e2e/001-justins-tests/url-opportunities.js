5d18ad82a9b01362b88a1f8292b37e3a018f199d
Justin Johnson <justin@mnjustin.com>
2025-08-17 10:40:30 -0500

    rename test file

--- cypress/e2e/001-justins-tests/url-opportunities.js
+++ cypress/e2e/001-justins-tests/url-opportunities.js
@@ -1,4 +1,6 @@
 describe('Page Title Test', () => {
+
+  // homepage is working
   it('should display "Coming Soon!" in the page title element', () => {
     cy.visit('https://www.sdjustin.com/index.cfm')
     cy.get('[data-testid="pagetitle"]')
@@ -6,13 +8,20 @@ describe('Page Title Test', () => {
       .should('contain.text', 'Coming Soon!')
   })
 
-  
+  // https://github.com/mnjustin/z/issues/251
+  it('make index.cfm the default index page when no page is specified', () => {
+    cy.visit('https://www.sdjustin.com')
+    cy.get('[data-testid="pagetitle"]')
+      .should("exist")
+      .should('contain.text', 'Coming Soon!')
+  })  
+
+  // https://github.com/mnjustin/z/issues/252
   it('should append the .cfm behind the scenes, making an seo friendly URL', () => {
     cy.visit('https://www.sdjustin.com/test')
     cy.get('[data-testid="pagetitle"]')
       .should("exist")
       .should('contain.text', 'test page')
   })
-
 
 })
